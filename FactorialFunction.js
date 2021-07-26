8ae97e33278c59bec17efa74048eeca5441de94c
Dan Chu <58838274+Mo1e@users.noreply.github.com>
2021-07-26 23:15:25 +0500

    Update FactorialFunction.js

--- FactorialFunction.js
+++ FactorialFunction.js
@@ -16,3 +16,19 @@ function factorial(a) {
 let array = makeArray(5)
 console.log(array)
 console.log(factorial(array))
+
+/* simplified */
+
+function factorial() {
+let a = +prompt("Найти факториал для ... (введите целое число)")
+let b = [...Array(a+1).keys()]
+let fact = b[2];
+  if (b.length < 3) {
+    fact = 1;
+  }else{
+    for (let i = 2; i < b.length - 1; i++) {
+      fact = fact * b[i+1];
+      }
+  }alert(`Факториал ${a} равен ${fact}`);
+  return(fact);
+}
